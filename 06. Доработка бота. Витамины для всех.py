c1429d05a2d2ff1b21c140458a9f1cd1606af156
Роман <romanmen81@gmail.com>
2024-12-15 11:31:36 +0500

    Update 06. Доработка бота. Витамины для всех.py

--- 06. Доработка бота. Витамины для всех.py
+++ 06. Доработка бота. Витамины для всех.py
@@ -16,7 +16,7 @@ class UserState(StatesGroup):
     weight = State()  # Состояние для ввода веса
 
 # Инициализация бота и диспетчера
-bot = Bot(token='7542914141:AAEGE-IRdCWrwjhrFbppasBq6gmTKcU707w')  # Замените YOUR_BOT_TOKEN на ваш токен
+bot = Bot(token='YOUR_BOT_TOKEN')  # Замените YOUR_BOT_TOKEN на ваш токен
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
